5d9e801bf1fcc3867fef4426629e723d1c7c6e96
Tudosie Razvan <razvanmariustudosie25@gmail.com>
2024-04-06 16:20:18 +0300

    added more installs

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 python3 -m venv .venv
 
 pip install transformers[rag]
+pip install fitz
+pip install tqdm
+pip install random
+pip install pandas
+pip install spacy
+pip install -U torch 
+pip install PyMuPDF 
+pip install sentence-transformers 
+pip install accelerate 
+pip install bitsandbytes 
+pip install flash-attn --no-build-isolation 
+
